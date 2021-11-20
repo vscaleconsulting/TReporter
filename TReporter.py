841df22b0d0b860a46241c95d216329d2ba00010
swapnil shinde <swapnilshinde9382@gmail.com>
2021-11-20 10:15:10 +0530

    added structured code

---
+++ TReporter.py
@@ -0,0 +1,87 @@
+from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.messages import GetMessagesRequest
+from telethon.tl.types import PeerChat
+import gspread
+import config
+
+url = config.spreadsheet_url
+gc = gspread.service_account(filename=config.spread_user_cred_json_file_addr)
+gsheet = gc.open_by_url(url)
+worksheet = gsheet.worksheets()[0]
+
+
+client = TelegramClient(config.name,config.api_id,config.api_hash)
+
+
+def insert_spread(values=[]):
+    if len(values)!=0:
+        worksheet.append_row(values)
+        print("values appended")
+        return 
+    
+    print("empty values passed")
+    
+async def filter_and_submit_report(usernames,message):
+    try:
+        message_posted_by_username = await client.get_entity(message.from_id.user_id)
+        for usr in usernames:
+                if usr.lower()==message_posted_by_username.username.lower():
+                    message_text = message.message
+                    message_channel = await client.get_entity(message.peer_id.channel_id)
+                    message_date = message.date.date()
+                    
+                    # print("text: ",message_text)
+                    # print("username: ",message_posted_by_username.username)
+                    # print("channel: ",message_channel.username)
+                    # print("date: ",message_date)
+                    # print("-----------------")
+
+                    to_append = [f"{message_date}",usr,message_channel.username,message_text] 
+                    insert_spread(to_append)
+    except Exception as e:
+        print("user does not exist")
+
+
+async def t_scrapper(starting_link,ending_link,usernames=[]):
+    print(starting_link,ending_link)
+    channel_name,starting_message_id = starting_link.split("/")[-2],starting_link.split("/")[-1]
+    ending_message_id = ending_link.split("/")[-1]
+
+    await client(JoinChannelRequest(channel_name))
+    channel_entity = await client.get_entity(channel_name)
+
+    messages = client.iter_messages(
+        channel_entity,
+        limit=1000,
+        min_id=int(starting_message_id),
+        max_id=int(ending_message_id),
+        # reverse=True
+    )
+
+    async for message in messages:
+        await filter_and_submit_report(usernames,message)
+
+def get_links(url):
+    gc = gspread.service_account(filename=config.spread_user_cred_json_file_addr)
+    gsheet = gc.open_by_url(url)
+    in_worksheet = gsheet.sheet1
+
+    return in_worksheet.col_values(1) , in_worksheet.col_values(2)
+
+    
+
+async def main():
+
+    starting_links,ending_links = get_links(config.fetch_spreadsheet_url)
+    usernames = config.usernames
+    
+    for i in range(len(starting_links)):
+        await t_scrapper(starting_links[i], ending_links[i],usernames)
+    
+
+
+with client:
+    client.loop.run_until_complete(main())
+
---
+++ config.py
@@ -0,0 +1,12 @@
+api_id = "13321231"
+api_hash = "87fab454b4d6eb4429017cd45ee64cc8"
+bot_token = "2145534514:AAFouNt214VBLN1nlzqeZZnMQPVDRhPfWhw"
+name = 'cyberspoof'
+phone = "+919382084913"
+
+
+spreadsheet_url = "https://docs.google.com/spreadsheets/d/1st6Hlbw0HUjk083ZQ5BfTGa8W27WQu6Lcv7nr9XbTA0/edit#gid=0"
+spread_user_cred_json_file_addr = "cred.json"
+
+fetch_spreadsheet_url = "https://docs.google.com/spreadsheets/d/1ROaeVV4a__XnKvSktAky0D3H6uD282Mc3gAzvFn3_yw/edit?usp=sharing"
+usernames = ["btczoro","bitsats","azm007","Yasar92","Pinkman000","Onlygems","btcluffy","Crypto_raja1","btcNoor","cry77"]
